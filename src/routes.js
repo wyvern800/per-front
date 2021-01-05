e283c857eabc359bf72d996a50216e8b7dbfd40a
Matheus Ferreira <matheusguiferreira@hotmail.com>
2021-01-05 08:46:44 -0300

    Character route

--- src/routes.js
+++ src/routes.js
@@ -6,11 +6,12 @@ import About from './pages/About';
 import Character from './pages/Character';
 
 export default function Routes() {
+  
   return (
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/characters" component={Character} />
-      <Route path="/characters/:id?" component={About} />
+      <Route path="/sebo/:id" component={About} />
     </Switch>
   );
 }
