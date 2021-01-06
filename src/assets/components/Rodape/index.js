2949ae9f77562ccfbb915095a670360b78cf2166
th3mike <maarcio_lacerda@hotmail.com>
2021-01-05 21:50:36 -0300

    sapo

--- src/assets/components/Rodape/index.js
+++ src/assets/components/Rodape/index.js
@@ -4,15 +4,44 @@ export default class Rodape extends Component {
   render() {
     return (
       <>
-        <footer className="poro-footer">
-          <section className="container">
-            <div className="poro-footer_content">imagem</div>
-            <div>
-              <span>Poro GG suck my limbs</span>
-              <a href="#">Salonphas</a>
+    <div className="footer-clean">
+        <footer>
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-sm-4 col-md-3 item">
+                        <h3>Services</h3>
+                        <ul>
+                            <li><a href="#">Web design</a></li>
+                            <li><a href="#">Development</a></li>
+                            <li><a href="#">Hosting</a></li>
+                        </ul>
+                    </div>
+                    <div className="col-sm-4 col-md-3 item">
+                        <h3>About</h3>
+                        <ul>
+                            <li><a href="#">Company</a></li>
+                            <li><a href="#">Team</a></li>
+                            <li><a href="#">Legacy</a></li>
+                        </ul>
+                    </div>
+                    <div className="col-sm-4 col-md-3 item">
+                        <h3>Careers</h3>
+                        <ul>
+                            <li><a href="#">Job openings</a></li>
+                            <li><a href="#">Employee success</a></li>
+                            <li><a href="#">Benefits</a></li>
+                        </ul>
+                    </div>
+                    <div className="col-lg-3 item social">
+                      <a href="#"><i className="bi bi-facebook"></i></a>
+                      <a href="#"><i className="bi bi-twitter"></i></a>
+                      <a href="#"><i className="bi bi-instagram"></i></a>
+                        <p className="copyright">Company Name © 2018</p>
+                    </div>
+                </div>
             </div>
-          </section>
         </footer>
+    </div>
       </>
     );
   }
