3b1696f3541efe9a3e56e5345557d0b97212eeeb
Matheus Ferreira <matheusguiferreira@hotmail.com>
2021-01-06 00:21:49 -0300

    Rodapé

--- src/assets/components/Rodape/index.js
+++ src/assets/components/Rodape/index.js
@@ -7,66 +7,64 @@ export default class Rodape extends Component {
     return (
       <>
         <div className="footer-clean">
-
-            <div className="container">
-              <div className="row justify-content-center">
-                <div className="col-sm-4 col-md-3 item">
-                  <h3>Services</h3>
-                  <ul>
-                    <li>
-                      <a href="#">Web design</a>
-                    </li>
-                    <li>
-                      <a href="#">Development</a>
-                    </li>
-                    <li>
-                      <a href="#">Hosting</a>
-                    </li>
-                  </ul>
-                </div>
-                <div className="col-sm-4 col-md-3 item">
-                  <h3>About</h3>
-                  <ul>
-                    <li>
-                      <a href="#">Company</a>
-                    </li>
-                    <li>
-                      <a href="#">Team</a>
-                    </li>
-                    <li>
-                      <a href="#">Legacy</a>
-                    </li>
-                  </ul>
-                </div>
-                <div className="col-sm-4 col-md-3 item">
-                  <h3>Careers</h3>
-                  <ul>
-                    <li>
-                      <a href="#">Job openings</a>
-                    </li>
-                    <li>
-                      <a href="#">Employee success</a>
-                    </li>
-                    <li>
-                      <a href="#">Benefits</a>
-                    </li>
-                  </ul>
-                </div>
-                <div className="col-lg-3 item social">
-                  <a href="#">
-                  <FaGithubAlt/>
-                  </a>
-                  <a href="#">
-                    <FaFacebookF />
-                  </a>
-                  <a href="#">
-                    <FaPatreon />
-                  </a>
-                  <p className="copyright">Company Name © 2018</p>
-                </div>
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-sm-4 col-md-3 item">
+                <h3>Services</h3>
+                <ul>
+                  <li>
+                    <a href="#">Web design</a>
+                  </li>
+                  <li>
+                    <a href="#">Development</a>
+                  </li>
+                  <li>
+                    <a href="#">Hosting</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-sm-4 col-md-3 item">
+                <h3>About</h3>
+                <ul>
+                  <li>
+                    <a href="#">Company</a>
+                  </li>
+                  <li>
+                    <a href="#">Team</a>
+                  </li>
+                  <li>
+                    <a href="#">Legacy</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-sm-4 col-md-3 item">
+                <h3>Developers</h3>
+                <ul>
+                  <li>
+                    <a href="#">Matheus Ferreira</a>
+                  </li>
+                  <li>
+                    <a href="#">Marcio Lacerda</a>
+                  </li>
+                  <li>
+                    <a href="#">Benefits</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-lg-3 item social">
+                <a href="#">
+                  <FaGithubAlt />
+                </a>
+                <a href="#">
+                  <FaFacebookF />
+                </a>
+                <a href="#">
+                  <FaPatreon />
+                </a>
+                <p className="copyright">Dharoka © 2020</p>
               </div>
             </div>
-
+          </div>
         </div>
       </>
     );
