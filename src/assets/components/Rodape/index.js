674dc310ca3945897503cfcb04f1380035fc8652
Matheus Ferreira <matheusguiferreira@hotmail.com>
2021-01-05 22:50:18 -0300

    Changed some things

--- src/assets/components/Rodape/index.js
+++ src/assets/components/Rodape/index.js
@@ -4,44 +4,68 @@ export default class Rodape extends Component {
   render() {
     return (
       <>
-    <div className="footer-clean">
-        <footer>
+        <div className="footer-clean">
+
             <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-sm-4 col-md-3 item">
-                        <h3>Services</h3>
-                        <ul>
-                            <li><a href="#">Web design</a></li>
-                            <li><a href="#">Development</a></li>
-                            <li><a href="#">Hosting</a></li>
-                        </ul>
-                    </div>
-                    <div className="col-sm-4 col-md-3 item">
-                        <h3>About</h3>
-                        <ul>
-                            <li><a href="#">Company</a></li>
-                            <li><a href="#">Team</a></li>
-                            <li><a href="#">Legacy</a></li>
-                        </ul>
-                    </div>
-                    <div className="col-sm-4 col-md-3 item">
-                        <h3>Careers</h3>
-                        <ul>
-                            <li><a href="#">Job openings</a></li>
-                            <li><a href="#">Employee success</a></li>
-                            <li><a href="#">Benefits</a></li>
-                        </ul>
-                    </div>
-                    <div className="col-lg-3 item social">
-                      <a href="#"><i className="bi bi-facebook"></i></a>
-                      <a href="#"><i className="bi bi-twitter"></i></a>
-                      <a href="#"><i className="bi bi-instagram"></i></a>
-                        <p className="copyright">Company Name © 2018</p>
-                    </div>
+              <div className="row justify-content-center">
+                <div className="col-sm-4 col-md-3 item">
+                  <h3>Services</h3>
+                  <ul>
+                    <li>
+                      <a href="#">Web design</a>
+                    </li>
+                    <li>
+                      <a href="#">Development</a>
+                    </li>
+                    <li>
+                      <a href="#">Hosting</a>
+                    </li>
+                  </ul>
                 </div>
+                <div className="col-sm-4 col-md-3 item">
+                  <h3>About</h3>
+                  <ul>
+                    <li>
+                      <a href="#">Company</a>
+                    </li>
+                    <li>
+                      <a href="#">Team</a>
+                    </li>
+                    <li>
+                      <a href="#">Legacy</a>
+                    </li>
+                  </ul>
+                </div>
+                <div className="col-sm-4 col-md-3 item">
+                  <h3>Careers</h3>
+                  <ul>
+                    <li>
+                      <a href="#">Job openings</a>
+                    </li>
+                    <li>
+                      <a href="#">Employee success</a>
+                    </li>
+                    <li>
+                      <a href="#">Benefits</a>
+                    </li>
+                  </ul>
+                </div>
+                <div className="col-lg-3 item social">
+                  <a href="#">
+                    <i className="bi bi-facebook"></i>
+                  </a>
+                  <a href="#">
+                    <i className="bi bi-twitter"></i>
+                  </a>
+                  <a href="#">
+                    <i className="bi bi-instagram"></i>
+                  </a>
+                  <p className="copyright">Company Name © 2018</p>
+                </div>
+              </div>
             </div>
-        </footer>
-    </div>
+
+        </div>
       </>
     );
   }
