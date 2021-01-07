58ebf7c717a7a07ab6a48d0ee692f9b2e0ae5ed6
th3mike <maarcio_lacerda@hotmail.com>
2021-01-07 04:40:31 -0300

    grid area

--- src/assets/components/Map/index.js
+++ src/assets/components/Map/index.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import { DivMap } from './styles';
-import imgmap from '../../../assets/map.png';
+import Badge from 'react-bootstrap/Badge';
+import Container from 'react-bootstrap/Container'
 
 export default class Map extends Component {
-    render() {
-        return (
-            <>
-            <DivMap>
-                BUCETA
-            <img src={imgmap} />
-            </DivMap>
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <DivMap>
+        <Container>
+          <Badge className="badge" className="a">1</Badge>
+          <Badge className="badge" className="b">2</Badge>
+          <Badge className="badge" className="c">3</Badge>
+          <Badge className="badge" className="d">4</Badge>
+          <Badge className="badge" className="e">4</Badge>
+          </Container>
+        </DivMap>
+      </>
+    );
+  }
 }
--- src/assets/components/Map/styles.js
+++ src/assets/components/Map/styles.js
@@ -1,5 +1,32 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import imgmap from '../../../assets/map.png';
 
-export const DivMap = styled.div `
-
+export const DivMap = styled.div`
+  div {
+    background-image: url(${imgmap});
+  background-repeat: no-repeat;
+    display: grid;
+    grid-template-columns: 31px 31px 31px 31px 31px 31px 31px 31px 31px 31px;
+    grid-template-rows: 100px 100px 100px;
+    gap: 0px 0px;
+    grid-template-areas:
+      '. . . . . . . . . .'
+      'b . . . . . . . . .'
+      '. . . . . . . . . .'
+      '. . . . . . . . . .';
+      .badge {
+        width: 25px;
+        height: 30px;
+        text-align: center;
+        font-size: 20px;
+        color: red;
+        background-color: white;
+      }
+      .a {
+        grid-area: a;
+      }
+      .b {
+        grid-area: b;
+      }
+  }
 `;
