d21c2a36e5889aa2217d082f7e039a3abf52b7e7
wyvern800 <matheusguiferreira@hotmail.com>
2021-07-26 00:21:09 -0300

    fixed

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  /*baseURL: 'http://localhost:8081/'*/  baseURL: 'https://project-eternal-return.herokuapp.com/'
+  baseURL: 'https://project-eternal-return.herokuapp.com/'
 });
 
 export default api;
